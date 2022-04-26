950a61b6e8dbebbff8b76b66e8d257dbe2b122ac
PiyankaBadole <94530938+PiyankaBadole@users.noreply.github.com>
2022-04-26 13:52:09 +0530

    Add files via upload

---
+++ task4/Q2/script.js
@@ -0,0 +1,57 @@
+
+   
+let myResume={
+    "basics": {
+      "name": "Priyanka Badole",
+      "email": "priya.badooe69@gamil.com",
+      "phone": 9172637746,
+      "degree": "B.E",
+      "location": {
+        "address": "161,untkhana,medical square",
+        "postalCode": 440009,
+        "city": "Nagpur",
+        "state": "Maharashtra",
+        "country": "India"
+      },
+      "profiles": 
+         {
+          "email": "priya.badole69@gmail.com",
+          "github":"https://github.com/PiyankaBadole"
+         }},
+    "work": [
+      {
+        "company": "The Photophilehub PVT",
+        "position": "Business Executive",
+        "startDate": "2020-11-04",
+        "endDate": "2021-08-15",
+        "summary": "i am the business executive handling various task and gaining lots of experience",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Priydarshini College Of Engineering",
+        "department": "computer technology",
+        "University":"RTMNU",
+        "studyType": "fulltime",
+       
+      }
+    ],
+    "skills": [
+      {
+        "name": "C,javascript",
+        "level": "beginer",
+       
+      }
+    ],
+    "languages": [
+      {
+        "language": "Hindi,Marathi,English",
+      }
+    ],
+    "interests": [
+      {
+        "name": "travelling,reading"
+      }
+    ]
+  }
+  console.log(myResume);
